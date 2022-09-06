6e395c5a8cdf0fa9459ba5ef09a02d7ab9624da2
Carlos Mocholí <carlossmocholi@gmail.com>
2022-09-06 15:35:29 +0200

    mypy

--- src/lightning_utilities/core/inheritance.py
+++ src/lightning_utilities/core/inheritance.py
@@ -2,7 +2,7 @@ from typing import Iterator, Set, Type
 
 
 def get_all_subclasses_iterator(cls: Type) -> Iterator[Type]:
-    def recurse(cl: Type) -> None:
+    def recurse(cl: Type) -> Iterator[Type]:
         for subclass in cl.__subclasses__():
             yield subclass
             yield from recurse(subclass)
