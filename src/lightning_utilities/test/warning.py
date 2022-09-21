67fc203a1b1938258571772972319bdb0b3b7840
Carlos Mocholí <carlossmocholi@gmail.com>
2022-09-21 20:06:04 +0200

    Update src/lightning_utilities/test/warning.py

--- src/lightning_utilities/test/warning.py
+++ src/lightning_utilities/test/warning.py
@@ -11,4 +11,4 @@ def no_warning_call(expected_warning: Type[Warning] = Warning, match: Optional[s
 
     for w in record:
         if issubclass(w.category, expected_warning) and (match is None or re.compile(match).search(str(w.message))):
-            raise AssertionError(f"{expected_warning.__name__} was raised: {w.message!r}")
+            raise AssertionError(f"`{expected_warning.__name__}` was raised: {w.message!r}")
