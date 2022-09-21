e0d47a78f309ff9114586f75b322114daef4675f
Carlos Mocholí <carlossmocholi@gmail.com>
2022-09-21 19:16:07 +0200

    Address mypy and pre-commit

--- src/lightning_utilities/test/warning.py
+++ src/lightning_utilities/test/warning.py
@@ -1,7 +1,7 @@
 import re
 import warnings
 from contextlib import contextmanager
-from typing import Type, Optional, Generator
+from typing import Generator, Optional, Type
 
 
 @contextmanager
@@ -10,6 +10,6 @@ def no_warning_call(expected_warning: Type[Warning] = Warning, match: Optional[s
         yield
 
     for w in record:
-        if issubclass(w.category, expected_warning) and (match is None or re.compile(match).search(w.message.args[0])):
+        if issubclass(w.category, expected_warning) and (match is None or re.compile(match).search(str(w.message))):
             msg = "A warning" if expected_warning is None else f"`{expected_warning.__name__}`"
             raise AssertionError(f"{msg} was raised: {w.message!r}")
