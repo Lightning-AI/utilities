848c31d13853f1eb30e0b4e2eb3756e822f12018
Jirka <jirka.borovec@seznam.cz>
2023-02-14 13:52:15 +0100

    warning

--- src/lightning_utilities/core/enums.py
+++ src/lightning_utilities/core/enums.py
@@ -2,6 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
+import warnings
 from enum import Enum
 from typing import List, Optional
 
