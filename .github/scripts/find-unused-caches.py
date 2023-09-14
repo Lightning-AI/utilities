e2510d3e62888c286614e52e1743f8309925cb6d
Jirka <jirka.borovec@seznam.cz>
2023-09-15 00:05:33 +0200

    fixing & cleaning

---
+++ .github/scripts/find-unused-caches.py
@@ -0,0 +1,62 @@
+import os
+from datetime import timedelta
+from typing import List
+
+import fire
+import pandas
+import requests
+
+
+def fetch_all_caches(repository: str, token: str, per_page: int = 100, max_pages: int = 100) -> List[dict]:
+    # Initialize variables for pagination
+    all_caches = []
+
+    for page in range(max_pages):
+        # Get a page of caches for the repository
+        url = f"https://api.github.com/repos/{repository}/actions/caches?page={page + 1}&per_page={per_page}"
+        headers = {"Authorization": f"token {token}"}
+        response = requests.get(url, headers=headers).json()
+        print(f"fetching page... {page} with {per_page} items of expected {response.get('total_count')}")
+        caches = response.get("actions_caches", [])
+
+        # Append the caches from this page to the overall list
+        all_caches.extend(caches)
+
+        # Check if there are more pages to retrieve
+        if len(caches) < per_page:
+            break
+
+    # Iterate through all caches and list them
+    if all_caches:
+        current_date = pandas.Timestamp.now(tz="UTC")
+        print(f"Caches {len(all_caches)} for {repository}:")
+        for cache in all_caches:
+            cache_key = cache["id"]
+            created_at = pandas.to_datetime(cache["created_at"])
+            last_used_at = pandas.to_datetime(cache["last_accessed_at"])
+            cache["last_used_days"] = current_date - last_used_at
+            size = cache["size_in_bytes"] / (1024 * 1024)
+            print(
+                f"- Cache Key: {cache_key} |"
+                f" Created At: {created_at.strftime('%Y-%m-%d %H:%M')} |"
+                f" Used At: {last_used_at.strftime('%Y-%m-%d %H:%M')} [{cache['last_used_days']}] |"
+                f" Size: {size:.2f} MB"
+            )
+    else:
+        print("No caches found for the repository.")
+    return all_caches
+
+
+def main(repository: str, token: str, dalay_days: float = 7, output_file: str = "unused-cashes.txt"):
+    caches = fetch_all_caches(repository, token)
+
+    delta_days = timedelta(days=dalay_days)
+    old_caches = [cache["id"] for cache in caches if cache["last_used_days"] > delta_days]
+    print(f"found {len(old_caches)} old caches:\n {old_caches}")
+
+    with open(output_file, "w", encoding="utf8") as fw:
+        fw.write(os.linesep.join(old_caches))
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
