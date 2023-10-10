4a2963b46a9fa8f100baa5b92fea61cbd14e054d
Jirka <jirka.borovec@seznam.cz>
2023-10-10 10:36:17 +0200

    encode

--- scripts/adjust-torch-versions.py
+++ scripts/adjust-torch-versions.py
@@ -59,7 +59,7 @@ def adjust(requires: List[str], pytorch_version: Optional[str] = None) -> List[s
     options = find_latest(pytorch_version)
     logging.debug(f"determined ecosystem alignment: {options}")
     for req in requires:
-        req_split = req.strip().strip().split("#", maxsplit=1)
+        req_split = req.strip().split("#", maxsplit=1)
         # anything before fst # shall be requirements
         req = req_split[0].strip()
         # anything after # in the line is comment
@@ -85,14 +85,14 @@ def _offset_print(reqs: List[str], offset: str = "\t|\t") -> str:
 def main(requirements_path: str, torch_version: Optional[str] = None) -> None:
     """The main entry point with mapping to the CLI for positional arguments only."""
     # rU - universal line ending - https://stackoverflow.com/a/2717154/4521646
-    with open(requirements_path, newline=os.linesep) as fopen:
+    with open(requirements_path, encoding="utf8") as fopen:
         requirements = fopen.readlines()
     requirements = adjust(requirements, torch_version)
     logging.info(
         f"requirements_path='{requirements_path}' with arg torch_version='{torch_version}' >>\n"
         f"{_offset_print(requirements)}"
     )
-    with open(requirements_path, "w") as fopen:
+    with open(requirements_path, "w", encoding="utf8") as fopen:
         fopen.writelines([r + os.linesep for r in requirements])
 
 
