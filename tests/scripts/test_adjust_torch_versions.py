fece87edc5c1a4d687d51109b22e2239396ab7ef
Jirka <jirka.borovec@seznam.cz>
2023-10-10 06:29:17 +0200

    ci

--- tests/scripts/test_adjust_torch_versions.py
+++ tests/scripts/test_adjust_torch_versions.py
@@ -9,6 +9,9 @@ def test_adjust_torch_versions_call():
     path_script = os.path.join(_PATH_SCRIPTS, "adjust-torch-versions.py")
     path_req_file = os.path.join(_PATH_DATA, "req_input.txt")
     path_expectation = os.path.join(_PATH_DATA, "req_expected.txt")
+    # save the input so it can be restored in test end
+    with open(path_req_file) as fopen:
+        req_input = fopen.read()
 
     return_code = subprocess.call([sys.executable, path_script, path_req_file, "1.10.0"])  # noqa: S603
     assert return_code == 0
@@ -17,3 +20,7 @@ def test_adjust_torch_versions_call():
     with open(path_expectation) as fopen:
         req_expected = fopen.read()
     assert req_result == req_expected
+
+    # restoring input data
+    with open(path_req_file, "w") as fopen:
+        fopen.write(req_input)
