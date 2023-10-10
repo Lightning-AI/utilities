f4f0b110b4c3ea4d69b9d0b63c842abeace86122
Jirka <jirka.borovec@seznam.cz>
2023-10-10 10:23:00 +0200

    refactor test

--- tests/scripts/__init__.py
+++ tests/scripts/__init__.py
@@ -3,4 +3,3 @@ import os
 _PATH_HERE = os.path.dirname(__file__)
 _PATH_ROOT = os.path.dirname(os.path.dirname(_PATH_HERE))
 _PATH_SCRIPTS = os.path.join(_PATH_ROOT, "scripts")
-_PATH_DATA = os.path.join(_PATH_HERE, "_data")
--- tests/scripts/test_adjust_torch_versions.py
+++ tests/scripts/test_adjust_torch_versions.py
@@ -6,30 +6,40 @@ from pathlib import Path
 
 import pytest
 
-from scripts import _PATH_DATA, _PATH_SCRIPTS
+from scripts import _PATH_SCRIPTS
+
+REQUIREMENTS_SAMPLE = """
+# This is sample requirements file
+#  with multi line comments
+
+torchvision >=0.13.0, <0.16.0  # sample # comment
+gym[classic_control] >=0.17.0, <0.27.0
+ipython[all] <8.15.0  # strict
+torchmetrics >=0.10.0, <1.3.0
+lightning-utilities >=0.8.0, <0.10.0
+
+"""
+REQUIREMENTS_EXPECTED = """
+# This is sample requirements file
+#  with multi line comments
+
+torchvision==0.11.1  # sample # comment
+gym[classic_control] >=0.17.0, <0.27.0
+ipython[all] <8.15.0  # strict
+torchmetrics >=0.10.0, <1.3.0
+lightning-utilities >=0.8.0, <0.10.0
+
+"""
 
 
-@pytest.fixture()
-def _backup_req_file(tmp_path) -> None:
-    """Fixture restore input file after test ends."""
-    req_file = "requirements_sample.txt"
-    # save the input, so it can be restored in test end
-    shutil.copy(Path(_PATH_DATA) / req_file, Path(tmp_path) / req_file)
-    # this is where the testing happens
-    yield
-    # Teardown: restoring input data
-    shutil.copy(Path(tmp_path) / req_file, Path(_PATH_DATA) / req_file)
-
-
-def test_adjust_torch_versions_call(_backup_req_file):  # noqa: PT019
+def test_adjust_torch_versions_call(tmp_path) -> None:
     path_script = os.path.join(_PATH_SCRIPTS, "adjust-torch-versions.py")
-    path_req_file = os.path.join(_PATH_DATA, "requirements_sample.txt")
-    path_expectation = os.path.join(_PATH_DATA, "requirements_expected.txt")
+    path_req_file = tmp_path / "requirements.txt"
+    with open(path_req_file, "w", encoding="utf8") as fopen:
+        fopen.write(REQUIREMENTS_SAMPLE)
 
     return_code = subprocess.call([sys.executable, path_script, path_req_file, "1.10.0"])  # noqa: S603
     assert return_code == 0
     with open(path_req_file, encoding="utf8") as fopen:
         req_result = fopen.read()
-    with open(path_expectation, encoding="utf8") as fopen:
-        req_expected = fopen.read()
-    assert req_result == req_expected
+    assert req_result == REQUIREMENTS_EXPECTED
