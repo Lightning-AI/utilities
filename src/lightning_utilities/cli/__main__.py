e25203316978ed723438accf8ae215e37fdc6dcc
Jirka B <j.borovec+github@gmail.com>
2024-11-19 10:06:55 +0100

    move fire

--- src/lightning_utilities/cli/__main__.py
+++ src/lightning_utilities/cli/__main__.py
@@ -2,13 +2,14 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-import fire
 
 from lightning_utilities.cli.dependencies import prune_pkgs_in_requirements, replace_oldest_ver
 
 
 def main() -> None:
     """CLI entry point."""
+    import fire
+
     fire.Fire({
         "requirements": {
             "prune-pkgs": prune_pkgs_in_requirements,
